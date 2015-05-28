22b14c0bd196596133b8d8f814723700b031a766
nigeldaniels <nigel.daniels@moovweb.com>
2015-05-27 21:00:55 -0700

    last change

--- src/main/java/com/netchosis/somthing/project2_phone2/Getusers.java
+++ src/main/java/com/netchosis/somthing/project2_phone2/Getusers.java
@@ -73,8 +73,6 @@ public class Getusers extends ListActivity {
             editor = store(settings,message);
         }
 
-
-
         whoami();
 
         Intent sintent = new Intent(this,sipwork.class); //intent for the sip background service
